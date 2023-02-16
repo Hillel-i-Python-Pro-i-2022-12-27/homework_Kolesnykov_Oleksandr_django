319a09f1470231c80894cd8d750774f5c38378cf
sashkakoleso88 <sashkakoleso88@gmail.com>
2023-02-17 01:05:14 +0200

    completed homework 10

--- apps/contacts/contacts_urls.py
+++ apps/contacts/contacts_urls.py
@@ -6,4 +6,8 @@ app_name = "contacts"
 
 urlpatterns = [
     path("contacts_list", views.list_of_contacts, name="contacts_list"),
+    path("contact_details/<int:pk>", views.ContactDetailView.as_view(), name="contact_detail"),
+    path("contact_create", views.ContactCreateView.as_view(), name="contact_create"),
+    path("contact_update/<int:pk>", views.ContactUpdateView.as_view(), name="contact_update"),
+    path("contact_delete/<int:pk>", views.ContactDeleteView.as_view(), name="contact_delete"),
 ]
--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,8 +1,46 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from apps.contacts.models import Contact
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 
 def list_of_contacts(request):
     return render(
-        request=request, template_name="contacts/contacts_list.html", context={"contacts": Contact.objects.all()}
+        request=request, template_name="contacts/contacts_list.html", context={"object_list": Contact.objects.all()}
     )
+
+
+class ContactListView(ListView):
+    model = Contact
+
+
+class ContactDetailView(DetailView):
+    model = Contact
+
+
+class ContactCreateView(CreateView):
+    model = Contact
+    fields = (
+        "user_name",
+        "phone_number",
+        "is_auto_generated",
+    )
+
+    success_url = reverse_lazy("contacts:contacts_list")
+
+
+class ContactUpdateView(UpdateView):
+    model = Contact
+    fields = (
+        "user_name",
+        "phone_number",
+        "is_auto_generated",
+    )
+
+    success_url = reverse_lazy("contacts:contacts_list")
+
+
+class ContactDeleteView(DeleteView):
+    model = Contact
+
+    success_url = reverse_lazy("contacts:contacts_list")
