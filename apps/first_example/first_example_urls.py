51ec6b9a3681876a09522167f3d2162db4b3ff92
sashkakoleso88 <sashkakoleso88@gmail.com>
2023-02-28 17:11:10 +0200

    added username display on all pages no logged users can't open "About me" page

--- apps/first_example/first_example_urls.py
+++ apps/first_example/first_example_urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 import apps.first_example.views as views
 
 app_name = "first_example"
 
 urlpatterns = [
-    path("greetings", views.greetings_with_name_and_age, name="greetings_default"),
+    path("greetings", login_required(views.greetings_with_name_and_age), name="greetings_default"),
     path("users_accounts", views.create_users_accounts, name="users_accounts"),
     path("users_accounts_amount/<int:amount>", views.create_users_accounts_amount, name="users_accounts_amount"),
 ]
